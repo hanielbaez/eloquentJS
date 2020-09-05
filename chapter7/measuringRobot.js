e7e9b0da93ba69f6d779e836919e5756f0f70cf3
NoobBaez <haniel_0101@hotmail.com>
2020-09-05 00:02:40 -0400

    Improve the robot speed

--- chapter7/measuringRobot.js
+++ chapter7/measuringRobot.js
@@ -131,13 +131,55 @@ function goalOrientedRobot({ place, parcels }, route) {
 //-----------------------------------------------------------
 function compareRobots(robot1, memory1, robot2, memory2) {
     const steps = { robot1: 0, robot2: 0 }
+    let best = Infinity;
 
     for (var simulation = 0; simulation < 100; simulation++) {
         const randomState = VillageState.random()
-        steps.robot1 += runRobot(randomState, robot1, memory1)
+        let result = runRobot(randomState, robot1, memory1)
+        best = result < best ? result : best;
+        steps.robot1 += result;
         steps.robot2 += runRobot(randomState, robot2, memory2)
     }
-    console.log(`Robot1 average steps: ${steps.robot1 / 100}, Robot2 average steps: ${steps.robot2 / 100}`)
+    console.log(`Robot1 average steps: ${steps.robot1 / 100}, Robot2 average steps: ${steps.robot2 / 100}  The best speed was ${best}`)
 }
 
-compareRobots(routeRobot, [], goalOrientedRobot, []);
+
+
+
+
+//compute all possible routes, and choose the closest one (12 steps/avgs)
+function shortRouteRobot({ place, parcels }, route) {
+    if (route.length == 0) {
+  
+      let distanceToPick = Infinity;
+      let distanceToDrop = Infinity;
+      let routeToPick, routeToDrop;
+  
+      parcels.forEach((parcel) => {
+        if (parcel.place != place) {
+          //pick up parcel
+          const routeFound = findRoute(roadGraph, place, parcel.place);
+          if (routeFound.length < distanceToPick) {
+            distanceToPick = routeFound.length;
+            routeToPick = routeFound;
+          }
+        }
+        else {
+          //drop parcel
+          const routeFound = findRoute(roadGraph, place, parcel.address);
+          if (routeFound.length < distanceToDrop) {
+            distanceToDrop = routeFound.length;
+            routeToDrop = routeFound;
+          }
+        }
+      })
+  
+      // Compare the distance between the shortest place to pick up a package 
+      // and the shortest place to drop off a package. 
+      // Given preference to pick up.
+      route = distanceToPick < distanceToDrop + 2 ? routeToPick : routeToDrop;
+    }
+    return { direction: route[0], memory: route.slice(1) };
+  }
+
+compareRobots(shortRouteRobot, [], goalOrientedRobot, []);
